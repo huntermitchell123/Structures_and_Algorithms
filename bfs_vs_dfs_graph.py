6368fd9b48da663e9f4b3b5663ddb033530e34da
Hunter Mitchell <huntermitchell@hunters-air.lan>
2020-10-29 14:24:36 -0700

    finished dfs iterative function

--- bfs_vs_dfs_graph.py
+++ bfs_vs_dfs_graph.py
@@ -17,10 +17,35 @@ class Graph:
     def add_edge(self,a,b):
         self.graph[a] = b
 
-    def print_graph_dfs_iter(self): # print the graph using depth first method iteratively
-        pass
+    def print_graph_dfs_iter(self,root): # print the graph using depth first method iteratively
+        temp = root
+        print(temp)
+        stack = [temp]
+        visited = [temp]
+        while (len(stack) != 0):
+            temp = stack[-1]
+            try: 
+                if (type(self.graph[temp]) == int): # if the next is one number
+                    if (self.graph[temp] not in stack and self.graph[temp] not in visited):
+                        stack.append(self.graph[temp])
+                        print(self.graph[temp])
+                        visited.append(self.graph[temp])
+                    else :
+                        stack.pop()
+                if (type(self.graph[temp]) == list): # if the next is multiple numbers
+                    for i in self.graph[temp]:
+                        if (i not in stack and i not in visited):
+                            stack.append(i)
+                            print(i)
+                            visited.append(i)
+                            break
+                        else :
+                            stack.pop()
+            except: # if there is no self.graph[temp]
+                stack.pop()
 
-    def print_graph_dfs_recur(self,curr): # print the graph using depth first method recursively
+
+    def print_graph_dfs_recur(self,curr): # print the graph using depth first method recursively - NOT DONE
 
         print(self.graph[curr])
         if self.graph[curr] != None:
@@ -28,7 +53,7 @@ class Graph:
                 self.print_bst_dfs_recur(next)
 
 
-    def print_graph_bfs(self): # print the graph using breadth first method
+    def print_graph_bfs(self): # print the graph using breadth first method - NOT DONE
         pass
 
 
@@ -36,14 +61,16 @@ class Graph:
 if __name__=="__main__":
 
     graph_edges = {
-        1:2, 2:[3,4], 3:5#, 5:1
+        1:2, 2:[3,4], 3:5, 5:1
     }
     g1 = Graph(edges=graph_edges)
     g1.add_edge(4,6)
 
     print(g1.graph)
-    print("Printing Graph using dfs recursively")
-    g1.print_graph_dfs_recur(1)
+    print("Printing Graph using dfs iteratively")
+    g1.print_graph_dfs_iter(1)
+    #print("Printing Graph using dfs recursively")
+    #g1.print_graph_dfs_recur(1)
 
 
 
